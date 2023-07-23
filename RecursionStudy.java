837f6d6852d169ec47bbe0e32fb867e3e3e848c8
Vamshi-Anantha <124914610+Vamshi-Anantha@users.noreply.github.com>
2023-07-23 23:25:49 +0530

    Add files via upload

---
+++ RecursionStudy.java
@@ -0,0 +1,14 @@
+// import java.util.*;
+public class RecursionStudy {
+    static void printDescending(int n) {
+        if (n < 1)
+            return;
+        System.out.println(n);
+        printDescending(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        printDescending(n);
+    }
+}
